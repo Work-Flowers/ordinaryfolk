f54bbb69797a9ee432b9f94c79de0694afbb89bc
Dennis Chiuten <dennis@work.flowers>
2025-01-17 17:35:25 +0800

    bunch of stuff

--- churn_analysis.R
+++ churn_analysis.R
@@ -25,9 +25,39 @@ saveRDS(df_raw, "raw.rds")
 # customer-level analysis -------------------------------------------------
 
 df_customers <- df_raw |> 
+  mutate(across(month_start, as.yearmon)) |> 
   group_by(region, customer_id, month_start, currency) |> 
   summarise(
-    across(monthly_recurring_revenue, sum), 
+    across(monthly_recurring_revenue, sum),
+    across(created_at, ~min(as.yearmon(.))),
     .groups = "drop"
     ) |> 
   mutate(active_flag = monthly_recurring_revenue > 0)
+
+
+df_cust_classified <- df_customers |> 
+  group_by(customer_id) |> 
+  arrange(month_start) |> 
+  mutate(
+    status = case_when(
+      month_start == created_at ~ "new",
+      monthly_recurring_revenue == 0 & lag(monthly_recurring_revenue, 1) > 0 ~ "churned",
+      monthly_recurring_revenue > lag(monthly_recurring_revenue, 1) ~ "expansion",
+      monthly_recurring_revenue < lag(monthly_recurring_revenue, 1) ~ "contraction",
+      monthly_recurring_revenue > 0 & month_start > created_at & is.na(lag(monthly_recurring_revenue, 1)) ~ "resurrected",
+      monthly_recurring_revenue > 0  ~ "retained"
+      ),
+    mrr_delta = monthly_recurring_revenue - coalesce(lag(monthly_recurring_revenue, 1), 0)
+    ) |> 
+  ungroup() |> 
+  arrange(customer_id, month_start)
+
+df_cust_summary <- df_cust_classified |> 
+  group_by(month_start, status) |> 
+  summarize(
+    n = n(),
+    mrr = sum(monthly_recurring_revenue),
+    across(mrr_delta, sum),
+    .groups = "drop"
+  )
+
