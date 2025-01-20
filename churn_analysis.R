8403ade9a49f1998ac30daa7ed8daf398da4842b
Dennis Chiuten <dennis@work.flowers>
2025-01-20 09:07:35 +0800

    incorporated dollar churn and MRR charts

--- churn_analysis.R
+++ churn_analysis.R
@@ -13,6 +13,7 @@ pacman::p_load_current_gh("denchiuten/tsViz")
 theme_set(theme_ts())
 
 billing <- "noah-e30be"
+bq_auth("dennis@work.flowers")
 # pull data ---------------------------------------------------------------
 
 sql <- "SELECT * FROM noah-e30be.all_stripe.subscription_history"
@@ -29,11 +30,11 @@ df_customers <- df_raw |>
   mutate(across(month_start, as.yearmon)) |> 
   group_by(region, customer_id, month_start, currency) |> 
   summarise(
-    across(monthly_recurring_revenue, sum),
+    across(mrr_usd, sum),
     across(created_at, ~min(as.yearmon(.))),
     .groups = "drop"
     ) |> 
-  mutate(active_flag = monthly_recurring_revenue > 0)
+  mutate(active_flag = mrr_usd > 0)
 
 
 df_cust_classified <- df_customers |> 
@@ -42,13 +43,13 @@ df_cust_classified <- df_customers |>
   mutate(
     status = case_when(
       month_start == created_at ~ "new",
-      monthly_recurring_revenue == 0 & lag(monthly_recurring_revenue, 1) > 0 ~ "churned",
-      monthly_recurring_revenue > 0 & month_start > created_at & lag(monthly_recurring_revenue, 1) == 0 ~ "reactivated",
-      monthly_recurring_revenue > lag(monthly_recurring_revenue, 1) ~ "mrr expansion",
-      monthly_recurring_revenue < lag(monthly_recurring_revenue, 1) ~ "mrr contraction",
-      monthly_recurring_revenue > 0  ~ "mrr flat"
+      mrr_usd == 0 & lag(mrr_usd, 1) > 0 ~ "churned",
+      mrr_usd > 0 & month_start > created_at & lag(mrr_usd, 1) == 0 ~ "reactivated",
+      mrr_usd > lag(mrr_usd, 1) ~ "mrr expansion",
+      mrr_usd < lag(mrr_usd, 1) ~ "mrr contraction",
+      mrr_usd > 0  ~ "mrr flat"
       ),
-    mrr_delta = monthly_recurring_revenue - coalesce(lag(monthly_recurring_revenue, 1), 0)
+    mrr_delta = mrr_usd - coalesce(lag(mrr_usd, 1), 0)
     ) |> 
   ungroup() |> 
   arrange(customer_id, month_start)
@@ -57,17 +58,16 @@ df_cust_summary <- df_cust_classified |>
   group_by(month_start, status) |> 
   summarize(
     n = n(),
-    mrr = sum(monthly_recurring_revenue),
-    across(mrr_delta, sum),
+    across(c(mrr_usd, mrr_delta), sum),
     .groups = "drop"
   ) |> 
   mutate(across(status, ~fct_relevel(., "new", "reactivated", "mrr expansion", "mrr contraction", "mrr flat")))
 
 df_total_actives <- df_cust_classified |> 
-  filter(monthly_recurring_revenue > 0) |> 
+  filter(mrr_usd > 0) |> 
   group_by(month_start) |> 
   summarize(
-    across(monthly_recurring_revenue, sum),
+    across(mrr_usd, sum),
     n = n()
   )
   
@@ -82,14 +82,14 @@ df_churn <- df_cust_summary |>
   arrange(month_start) |> 
   mutate(
     churn_rate = n_churned / lag(n, 1),
-    dollar_churn_rate = mrr_delta / lag(monthly_recurring_revenue, 1)
+    dollar_churn_rate = -mrr_delta / lag(mrr_usd, 1)
   )
 
 # plot customers ----------------------------------------------------------
 (
   p_actives <- df_cust_summary |> 
     filter(
-      mrr > 0,
+      mrr_usd > 0,
       month_start >= as.yearmon("2023-01")
       ) |> 
     ggplot(aes(month_start, n)) +
@@ -106,7 +106,28 @@ df_churn <- df_cust_summary |>
     )
 )
 
-p_churn <- df_churn |> 
+(
+  p_mrr <- df_cust_summary |> 
+    filter(
+      mrr_usd > 0,
+      month_start >= as.yearmon("2023-01")
+    ) |> 
+    ggplot(aes(month_start, mrr_usd)) +
+    geom_col(aes(fill = status)) +
+    scale_x_yearmon(format = "%b %y", n = 5) +
+    scale_y_continuous(labels = label_dollar()) +
+    PrettyCols::scale_fill_pretty_d("Bold") +
+    labs(
+      y = "Total MRR",
+      x = NULL,
+      fill = "Customer Type",
+      title = "Total MRR by Status",
+      subtitle = "Source: Stripe"
+    )
+)
+
+(
+  p_churn <- df_churn |> 
   filter(month_start >= as.yearmon("2023-01")) |> 
   ggplot(aes(month_start, churn_rate)) +
   geom_line(color = "blue") + 
@@ -118,3 +139,21 @@ p_churn <- df_churn |>
     title = "% of Customers Churning from Prior Month",
     subtitle = "Source: Stripe"
   )
+)
+
+(
+  p_dollar_churn <- df_churn |> 
+    filter(month_start >= as.yearmon("2023-01")) |> 
+    ggplot(aes(month_start, dollar_churn_rate)) +
+    geom_line(color = "blue") + 
+    scale_x_yearmon(format = "%b %y", n = 5) +
+    scale_y_continuous(labels = label_percent()) +
+    labs(
+      y = "Dollar Churn Rate",
+      x = NULL,
+      title = "% of MRR Churning from Prior Month",
+      subtitle = "Source: Stripe"
+    )
+)
+
+(p_actives + p_churn) / (p_mrr + p_dollar_churn)
