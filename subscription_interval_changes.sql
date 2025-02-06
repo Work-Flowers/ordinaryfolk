3013cfb57e93335b41b3d76028bb8ab56908e62c
Dennis Chiuten <dennis@work.flowers>
2025-02-06 11:06:38 +0800

    got rid of invoice table

--- subscription_interval_changes.sql
+++ subscription_interval_changes.sql
@@ -8,7 +8,6 @@ WITH intervals AS (
 		p.product_id,
 		prod.name AS product_name,
 		JSON_EXTRACT_SCALAR(prod.metadata, '$.condition') AS condition,
-		i.created AS invoice_date,
 		p.interval_count AS current_interval_count,
 		LAG(p.interval_count) OVER (
 			PARTITION BY sh.id, p.product_id
@@ -19,10 +18,8 @@ WITH intervals AS (
 	        ORDER BY sh._fivetran_start
 	    ) AS first_interval_count
 	FROM all_stripe.subscription_history AS sh
-	JOIN all_stripe.invoice AS i 
-		ON sh.latest_invoice_id = i.id
 	JOIN all_stripe.invoice_line_item AS ili 
-		ON i.id = ili.invoice_id
+		ON sh.latest_invoice_id = ili.invoice_id
 	JOIN all_stripe.plan AS p 
 		ON ili.plan_id = p.id
 		AND p.interval = 'month'
