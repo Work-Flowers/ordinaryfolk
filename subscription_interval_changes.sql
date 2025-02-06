1e79e798ab7fdc9b1cb1c70c5d0eb6364b6a2753
Dennis Chiuten <dennis@work.flowers>
2025-02-06 10:55:25 +0800

    additional dimensions for filtering

--- subscription_interval_changes.sql
+++ subscription_interval_changes.sql
@@ -1,10 +1,13 @@
 WITH intervals AS (
 	SELECT DISTINCT
+		UPPER(sh.region) AS country,
 		sh.id AS subscription_id,
 		DATE(sh.created) AS subscription_created,
 		DATE(sh._fivetran_start) AS subscription_updated,
 		p.id AS plan_id,
 		p.product_id,
+		prod.name AS product_name,
+		JSON_EXTRACT_SCALAR(prod.metadata, '$.condition') AS condition,
 		i.created AS invoice_date,
 		p.interval_count AS current_interval_count,
 		LAG(p.interval_count) OVER (
@@ -20,14 +23,16 @@ WITH intervals AS (
 		ON sh.latest_invoice_id = i.id
 	JOIN all_stripe.invoice_line_item AS ili 
 		ON i.id = ili.invoice_id
-	JOIN all_stripe.plan p 
+	JOIN all_stripe.plan AS p 
 		ON ili.plan_id = p.id
 		AND p.interval = 'month'
+	JOIN all_stripe.product AS prod
+		ON p.product_id = prod.id
 )
 
 SELECT 
-	DATE_DIFF(subscription_updated, subscription_created, DAY) AS days_elapsed,
-	intervals.*
+	intervals.*,
+	DATE_DIFF(subscription_updated, subscription_created, DAY) AS days_elapsed	
 FROM intervals
 WHERE 
 	1 = 1
@@ -35,6 +40,3 @@ WHERE
 	AND current_interval_count > previous_interval_count
 	AND current_interval_count > 3
 	AND DATE_DIFF(subscription_updated, subscription_created, DAY)  <= 90
-ORDER BY 
-	subscription_id,
-	subscription_updated
