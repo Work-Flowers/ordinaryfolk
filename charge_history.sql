4b1a508b391cadb3889fd4530ff084372c58484e
Dennis Chiuten <dennis@work.flowers>
2025-02-10 16:31:30 +0800

    added a bunch of dimensions for filtering

--- charge_history.sql
+++ charge_history.sql
@@ -1,15 +1,32 @@
 WITH monthly_charges AS (
-    SELECT
-        ch.region,
-        DATE(ch.created) AS created,
-        ch.customer_id,
-        MIN(MIN(DATE(ch.created))) OVER (PARTITION BY ch.customer_id) AS first_purchase,
-        SUM(ch.amount / fx.fx_to_usd / 100) AS amount
-    FROM all_stripe.charge ch
-    LEFT JOIN ref.fx_rates fx
-        ON ch.currency = fx.currency
-    WHERE ch.status = 'succeeded'
-    GROUP BY 1, 2, 3
+   SELECT
+		ch.created,
+		ch.region,
+		CASE 
+			WHEN ii.subscription_id IS NULL THEN 'One-Time'
+			ELSE 'Subscription'
+			END AS purchase_type,
+		ch.customer_id,
+		prod.id AS product_id,
+		prod.name AS product_name,
+		JSON_EXTRACT_SCALAR(prod.metadata, '$.condition') AS condition,
+		COALESCE(px.unit_amount, ch.amount) / fx.fx_to_usd / 100  AS amount,
+		MIN(ch.created) OVER(PARTITION BY ch.customer_id) AS first_purchase
+	FROM all_stripe.charge AS ch
+	LEFT JOIN all_stripe.payment_intent AS pi
+		ON ch.payment_intent_id = pi.id
+	INNER JOIN ref.fx_rates AS fx
+		ON ch.currency = fx.currency
+	LEFT JOIN all_stripe.invoice_line_item AS ii
+		ON ch.invoice_id = ii.invoice_id
+	LEFT JOIN all_stripe.price AS px
+	-- use price_id from invoice line item if available; otherwise look for price_id in payment_intent metadata
+		ON COALESCE(ii.price_id, JSON_EXTRACT(pi.metadata, '$.paymentIntentPriceId'), JSON_EXTRACT(pi.metadata, '$.stripePriceIds')) = px.id
+	LEFT JOIN all_stripe.product AS prod
+		ON px.product_id = prod.id
+	WHERE
+		ch.status = 'succeeded'
+	GROUP BY 1,2,3,4,5,6,7,8
 ),
 
 months AS (
@@ -25,52 +42,60 @@ months AS (
 ),
 
 all_customers AS (
-    SELECT 
+    SELECT DISTINCT
     	region,
+    	purchase_type,
     	customer_id,
-    	MIN(DATE(created)) AS first_purchase
-    FROM all_stripe.charge
-    WHERE status = 'succeeded'
-    GROUP BY 1,2
+    	product_id,
+    	product_name,
+    	condition, 
+    	first_purchase
+    FROM monthly_charges
 ),
 
 zero_filled_months AS (
     SELECT
-        c.region,
         m.created,
+        c.region,
+        c.purchase_type,
         c.customer_id,
-        c.first_purchase,
-        0.0 AS amount
+        c.product_id,
+        c.product_name,
+        c.condition,
+        0.0 AS amount,
+        DATE(c.first_purchase) AS first_purchase
     FROM months m
     CROSS JOIN all_customers c
 ),
 
 unioned AS (
 	SELECT 
-	    region,
-	    created,
-	    customer_id,
-	    first_purchase,
-	    amount
+		DATE(created) AS created,
+		region,
+		purchase_type,
+		customer_id,
+		product_id,
+		product_name,
+		condition,
+		amount,
+		DATE(first_purchase) AS first_purchase
 	FROM monthly_charges
 	
 	UNION ALL
 	
-	SELECT
-	    region,
-	    created,
-	    customer_id,
-	    first_purchase,
-	    amount
+	SELECT *
 	FROM zero_filled_months
 )
 
 SELECT
-	region,
 	DATE_TRUNC(created, MONTH) AS created,
-	customer_id,
 	DATE_TRUNC(first_purchase, MONTH) AS first_purchase,
+	region,
+	purchase_type,
+	customer_id,
+	product_name,
+	condition,
 	SUM(amount) AS amount
 FROM unioned
 WHERE DATE_TRUNC(created, MONTH) >= DATE_TRUNC(first_purchase, MONTH)
-GROUP BY 1,2,3,4
+GROUP BY 1,2,3,4,5,6,7
