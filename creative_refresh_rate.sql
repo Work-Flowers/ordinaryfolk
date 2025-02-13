c5db9d6fd8fc1cfbdaaa5d4ff93bfefbf2681251
denchiuten <d.chiuten@icloud.com>
2025-02-13 12:02:18 +0800

    creative_id level granularity

--- creative_refresh_rate.sql
+++ creative_refresh_rate.sql
@@ -1,17 +1,19 @@
 SELECT
     ba.date,
+    cr.id AS creative_id,
     CASE 
     	WHEN ch.objective = 'OUTCOME_SALES' THEN 'Sales'
     	WHEN ch.objective = 'OUTCOME_AWARENESS' THEN 'Awareness'
     	ELSE ch.objective
     	END AS objective,
-    COUNT(DISTINCT ah.id) AS ad_count,
-    COUNT(DISTINCT cr.id) AS creative_count
-FROM facebook_ads.basic_ad AS ba
-INNER JOIN facebook_ads.ad_history AS ah 
-	ON ba.ad_id = CAST(ah.id AS STRING)
-INNER JOIN facebook_ads.creative_history AS cr 
-	ON ah.creative_id = cr.id
-LEFT JOIN facebook_ads.campaign_history AS ch 
+    MIN(ba.date) OVER (PARTITION BY cr.id) AS create_date
+FROM facebook_ads.creative_history AS cr
+INNER JOIN facebook_ads.ad_history AS ah
+	ON cr.id = ah.creative_id
+INNER JOIN facebook_ads.basic_ad AS ba
+    ON CAST(ah.id AS STRING) = ba.ad_id
+    AND ba.impressions > 0 -- only include rows where impressions were served
+LEFT JOIN facebook_ads.campaign_history AS ch
 	ON ah.campaign_id = ch.id
-GROUP BY 1,2
+GROUP BY 1,2,3
+ORDER BY 2,1
