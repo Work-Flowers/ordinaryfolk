9f6bf01b893e0d6195055e9dd0cd254cf4bd8bd1
denchiuten <d.chiuten@icloud.com>
2025-02-20 16:36:51 +0800

    initial commit for checkout completion rate calc

---
+++ checkout_completions.sql
@@ -0,0 +1,14 @@
+WITH completed AS(
+	SELECT
+		checkout.country,
+		DATE(t.`timestamp`) AS date,
+		COUNT(DISTINCT checkout.order_id) AS n
+	FROM segment.checkout_completed AS checkout
+	INNER JOIN segment.tracks AS t
+		ON checkout.message_id = t.message_id
+	INNER JOIN segment.checkout_completed_products AS prod
+		ON checkout.message_id = prod.message_id
+		AND prod.condition = 'teleconsultation'
+	GROUP BY 1,2
+)
+
