2169b7070bf34773935c41671035c7ec4c9c22d8
denchiuten <d.chiuten@icloud.com>
2025-02-24 13:22:30 +0800

    added marketing spend

--- ltv_cac.sql
+++ ltv_cac.sql
@@ -21,6 +21,15 @@ churn_info AS (
 	FROM finance_metrics.customer_lifecyle_monthly
 	WHERE lifecyle = 'Churn'
 	GROUP BY 1,2,3
+),
+
+marketing AS (
+	SELECT
+		DATE_TRUNC(ms.date, MONTH) AS date,
+		LOWER(ms.country_code) AS region,
+		SUM(ms.cost_usd) AS cost
+	FROM cac.marketing_spend AS ms
+	GROUP BY 1,2
 )
 
 SELECT	
@@ -34,9 +43,13 @@ SELECT
 	LAG(am.current_mrr) OVER(
 		PARTITION BY am.region 
 		ORDER BY am.obs_date
-	) AS base_mrr
+	) AS base_mrr,
+	m.cost AS marketing_spend
 FROM all_mrr AS am
 LEFT JOIN churn_info AS ci
 	ON am.obs_date = ci.obs_date
 	AND am.region = ci.region
+LEFT JOIN marketing AS m
+	ON am.obs_date = m.date
+	AND am.region = m.region
 ORDER BY 1,2
