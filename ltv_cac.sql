f130681e7c57ed7c823a712767c91370060d528b
denchiuten <d.chiuten@icloud.com>
2025-02-24 13:19:09 +0800

    simplified by removing one of the CTEs

--- ltv_cac.sql
+++ ltv_cac.sql
@@ -5,7 +5,7 @@ WITH all_mrr AS (
 		region,
 		obs_date,
 		SUM(current_mrr) AS current_mrr,
-		COUNT(DISTINCT customer_id) AS current_n_customers
+		SUM(n_customers) AS current_n_customers
 	FROM finance_metrics.customer_lifecyle_monthly
 	WHERE 
 		current_mrr > 0
@@ -16,21 +16,27 @@ churn_info AS (
 	SELECT
 		region,
 		obs_date,
-		n_customers,
+		n_customers AS n_churned_customers,
 		SUM(lagged_mrr) AS churned_mrr		
 	FROM finance_metrics.customer_lifecyle_monthly
 	WHERE lifecyle = 'Churn'
 	GROUP BY 1,2,3
-),
-
-lagged_base AS (
-	SELECT
-		region,
-		obs_date,
-		LAG(SUM(n_customers)) OVER (PARTITION BY region ORDER BY obs_date) AS base_n_customers,
-		LAG(SUM(current_mrr)) OVER (PARTITION BY region ORDER BY obs_date) AS base_mrr
-	FROM finance_metrics.customer_lifecyle_monthly
-	WHERE lifecyle <> 'Churn'
-	GROUP BY 1,2
 )
 
+SELECT	
+	am.*,
+	ci.n_churned_customers,
+	ci.churned_mrr,
+	LAG(am.current_n_customers) OVER(
+		PARTITION BY am.region 
+		ORDER BY am.obs_date
+	) AS base_n_customers,
+	LAG(am.current_mrr) OVER(
+		PARTITION BY am.region 
+		ORDER BY am.obs_date
+	) AS base_mrr
+FROM all_mrr AS am
+LEFT JOIN churn_info AS ci
+	ON am.obs_date = ci.obs_date
+	AND am.region = ci.region
+ORDER BY 1,2
