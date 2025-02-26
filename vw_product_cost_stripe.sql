1bba0eb8b643f8482ea762d9289ac42c437d9662
denchiuten <d.chiuten@icloud.com>
2025-02-26 18:27:39 +0800

    added logic for gst_vat and cashback

--- vw_product_cost_stripe.sql
+++ vw_product_cost_stripe.sql
@@ -31,7 +31,13 @@ SELECT
 	px.id AS price_id,
 	px.currency,
 	mi.cost_box * COALESCE(CAST(JSON_EXTRACT_SCALAR(px.metadata, '$.boxes') AS FLOAT64), 1) AS cogs,
-	packaging_cost AS packaging
+	packaging_cost AS packaging,
+	CASE 
+		WHEN px.region = 'hk' THEN 0
+		WHEN px.region = 'sg' THEN 0.09
+		WHEN px.region = 'jp' THEN 0.10
+		END AS gst_vat,
+	.02 AS cashback
 FROM manual_inputs AS mi
 LEFT JOIN all_stripe.price AS px
 	ON mi.product_id = px.product_id
