7a597507e5570677ff95c7a8f43f204b38637b33
denchiuten <d.chiuten@icloud.com>
2025-02-27 13:57:50 +0800

    revised to use finance_metrics.monthly_contribution_margin view

--- cac_cm.sql
+++ cac_cm.sql
@@ -1,38 +1,10 @@
-WITH marketing AS (
-	SELECT
-		DATE_TRUNC(date, MONTH) AS date,
-		UPPER(country_code) AS country,
-		SUM(cost_usd) AS marketing_spend
-	FROM cac.marketing_spend
-	GROUP BY 1,2
-),
-
-acq_dates AS (
+WITH acq_dates AS (
 	SELECT
 		ch.region,
 		ch.customer_id,
 		MIN(DATE_TRUNC(ch.created, month)) AS acquired_date
 	FROM all_stripe.charge AS ch
 	WHERE ch.status = 'succeeded'
-	GROUP BY 1,2
-),
-
-cm AS (
-	SELECT
-		DATE_TRUNC(purchase_date, month) AS date,
-		region AS country,
-		SUM(COALESCE(line_item_amount_usd, total_charge_amount_usd)) AS revenue,
-		SUM(cogs) AS cogs,
-		SUM(gst_vat * COALESCE(line_item_amount_usd, total_charge_amount_usd)) AS gst_vat_usd,
-		SUM(refund_rate * COALESCE(line_item_amount_usd, total_charge_amount_usd)) AS refund_usd
-		
-	FROM finance_metrics.contribution_margin
-	
-	WHERE 
-		1 = 1
-		-- we only want to use subscription revenue for this calculation of contribution margin,
-		-- because we are applying the CM% to MRR in the final CAC payback period calc
-		AND purchase_type = 'Subscription'
 	GROUP BY 1,2
 ),
 
@@ -51,24 +23,27 @@ subscriptions AS (
 )
 
 SELECT
-	mar.date,
-	mar.country,
-	mar.marketing_spend,
-	cm.revenue,
+	cm.date,
+	cm.country,
+	cm.marketing_cost,
+	cm.amount AS revenue,
 	cm.cogs,
-	cm.gst_vat_usd,
-	cm.refund_usd,
+	cm.cashback,
+	cm.packaging,
+	cm.gst_vat,
+	cm.refunds,
+	cm.payment_gateway_fees,
+	cm.delivery_cost,
 	subscriptions.mrr,
 	subscriptions.total_subscribers,
 	COUNT(DISTINCT ad.customer_id) AS n_new_customers
-FROM marketing AS mar
+FROM finance_metrics.monthly_contribution_margin AS cm
 LEFT JOIN acq_dates AS ad
-	ON DATE(mar.date) = DATE(ad.acquired_date)
-	AND LOWER(mar.country) = LOWER(ad.region)
-LEFT JOIN cm
-	ON mar.date = cm.date
-	AND LOWER(mar.country) = LOWER(cm.country)
+	ON DATE(cm.date) = DATE(ad.acquired_date)
+	AND cm.country = ad.region
 LEFT JOIN subscriptions
-	ON mar.date = subscriptions.date
-	AND LOWER(mar.country) = LOWER(subscriptions.country)
-GROUP BY 1,2,3,4,5,6,7,8,9
+	ON cm.date = subscriptions.date
+	AND cm.country = subscriptions.country
+WHERE
+	cm.purchase_type = 'Subscription'
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13
