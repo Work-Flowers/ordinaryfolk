e9186e028d57537a00b13fa2edb0a5025e714edb
denchiuten <d.chiuten@icloud.com>
2025-03-04 10:47:44 +0800

    de-duping in case of customers with multiple active subscriptions

--- front_migration/stripe_customers.sql
+++ front_migration/stripe_customers.sql
@@ -5,6 +5,14 @@ WITH subs_status AS(
 		customer_id
 	FROM all_stripe.subscription_history
 	QUALIFY ROW_NUMBER() OVER(PARTITION BY id ORDER BY created DESC) = 1
+),
+
+subs_active AS (
+	SELECT DISTINCT 
+		customer_id
+	FROM subs_status
+	WHERE status = 'active'
+
 )
 
 SELECT
@@ -13,16 +21,15 @@ SELECT
 	cus.name,
 	cus.email,
 	cus.phone,
-	ss.subscription_id IS NOT NULL AS is_active_subscriber,
+	ss.customer_id IS NOT NULL AS is_active_subscriber,
 	MAX(ch.created) AS last_transaction_date
 FROM all_stripe.customer AS cus
 LEFT JOIN all_stripe.charge AS ch
 	ON cus.id = ch.customer_id
 	AND ch.status = 'succeeded'
-LEFT JOIN subs_status AS ss
+LEFT JOIN subs_active AS ss
 	ON cus.id = ss.customer_id
-	AND ss.status = 'active'
 WHERE
 	1 = 1
-	AND (ss.subscription_id IS NOT NULL OR ch.id IS NOT NULL)
+	AND (ss.customer_id IS NOT NULL OR ch.id IS NOT NULL)
 GROUP BY 1,2,3,4,5,6
