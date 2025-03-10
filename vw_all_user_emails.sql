755531ea10dd95818f204091e844a7d7dd5018d1
denchiuten <d.chiuten@icloud.com>
2025-03-10 14:12:57 +0800

    added utm_source

--- vw_all_user_emails.sql
+++ vw_all_user_emails.sql
@@ -3,7 +3,7 @@ CREATE VIEW all_postgres.user_emails AS
 
 SELECT
 	email,
-	sys_id
+	sys_id,
+	utm_source
 FROM all_postgres.user
 QUALIFY ROW_NUMBER() OVER(PARTITION BY email ORDER BY updated_at DESC) = 1
-
