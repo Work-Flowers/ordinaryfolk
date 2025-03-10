f74406550d4032ad1eed7d40717ccab32f6190ac
denchiuten <d.chiuten@icloud.com>
2025-03-10 14:11:17 +0800

    unique mappings of email address and internal sys_id

---
+++ vw_all_user_emails.sql
@@ -0,0 +1,9 @@
+DROP VIEW IF EXISTS all_postgres.user_emails;
+CREATE VIEW all_postgres.user_emails AS 
+
+SELECT
+	email,
+	sys_id
+FROM all_postgres.user
+QUALIFY ROW_NUMBER() OVER(PARTITION BY email ORDER BY updated_at DESC) = 1
+
