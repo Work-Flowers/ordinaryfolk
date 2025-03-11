86806f19b5670b56a2176476a1bf6f3e97f95c00
denchiuten <d.chiuten@icloud.com>
2025-03-11 18:39:51 +0800

    use contribution_margin view and included condition

--- vw_subscription_renewals.sql
+++ vw_subscription_renewals.sql
@@ -3,25 +3,21 @@ CREATE VIEW all_stripe.subscription_renewals AS
 
 WITH sub_starts AS (
 	SELECT DISTINCT
+		region,
 		created,
 		id AS subscription_id
 	FROM all_stripe.subscription_history
 )
 
 SELECT
+	sub_starts.region,
 	DATE(sub_starts.created) AS created_date,
 	sub_starts.subscription_id,
-	COUNT(DISTINCT ch.id) AS n_charges_in_first_90d,
-	SUM(ch.amount / fx.fx_to_usd / COALESCE(su.subunits, 100)) AS amount_usd_in_first_90d
+	cm.condition,
+	COUNT(DISTINCT cm.charge_id) AS n_charges_in_first_90d,
+	SUM(COALESCE(cm.line_item_amount_usd, cm.total_charge_amount_usd)) AS amount_usd_in_first_90d
 FROM sub_starts
-LEFT JOIN all_stripe.invoice AS inv
-	ON sub_starts.subscription_id = inv.subscription_id
-LEFT JOIN all_stripe.charge AS ch
-	ON inv.id = ch.invoice_id
-	AND DATE(ch.created) <= DATE_ADD(DATE(sub_starts.created), INTERVAL 90 DAY)
-	AND DATE(ch.created) > DATE(sub_starts.created)
-LEFT JOIN ref.fx_rates AS fx
-	ON inv.currency = fx.currency
-LEFT JOIN ref.stripe_currency_subunits AS su
-	ON inv.currency = su.currency
-GROUP BY 1,2
+LEFT JOIN finance_metrics.contribution_margin AS cm
+	ON cm.subscription_id = sub_starts.subscription_id
+	AND cm.purchase_date <= DATE_ADD(DATE(sub_starts.created), INTERVAL 90 DAY)
+GROUP BY 1,2,3,4
