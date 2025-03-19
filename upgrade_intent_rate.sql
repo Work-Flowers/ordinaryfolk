4020d0d5d55db2e84ca4811111aca2b8d2c509d5
denchiuten <d.chiuten@icloud.com>
2025-03-19 11:32:57 +0800

    added no of new subscription customers and used all_keys method

--- upgrade_intent_rate.sql
+++ upgrade_intent_rate.sql
@@ -18,14 +18,55 @@ upgrades AS (
 	INNER JOIN segment.tracks AS t
 		ON u.message_id = t.message_id
 	GROUP BY 1,2
+),
+
+new_customers AS (
+	SELECT DISTINCT
+		DATE(created) AS date,
+		region,
+		COUNT(DISTINCT customer_id) AS n_new_customers
+	FROM all_stripe.subscription_history	
+	WHERE status = 'active'
+	GROUP BY 1,2
+),
+
+all_keys AS (
+
+	SELECT DISTINCT
+		date,
+		region
+	FROM landing
+	
+	UNION DISTINCT
+	
+	SELECT DISTINCT
+		date,
+		region
+	FROM upgrades
+	
+	UNION DISTINCT
+	
+	SELECT DISTINCT
+		date,
+		region
+	FROM new_customers
 )
 
+
+
 SELECT
-	landing.date,
-	landing.region,
-	landing.n_users AS n_landing,
-	COALESCE(upgrades.n_users, 0) AS n_upgrades
-FROM landing
+	k.date,
+	k.region,
+	COALESCE(landing.n_users, 0) AS n_landing,
+	COALESCE(upgrades.n_users, 0) AS n_upgrades,
+	COALESCE(nc.n_new_customers, 0) AS n_new_customers
+FROM all_keys AS k
+LEFT JOIN landing
+	ON k.date = landing.date
+	AND k.region = landing.region
 LEFT JOIN upgrades
-	ON landing.date = upgrades.date
-	AND landing.region = upgrades.region
+	ON k.date = upgrades.date
+	AND k.region = upgrades.region
+LEFT JOIN new_customers AS nc
+	ON k.date = nc.date
+	AND k.region = nc.region
