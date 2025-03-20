2380704f212781d723f3f1d135d57448dddc64cd
denchiuten <d.chiuten@icloud.com>
2025-03-20 13:40:11 +0800

    added condition-level granularity to CTE and JOIN

--- vw_subscription_renewals.sql
+++ vw_subscription_renewals.sql
@@ -2,22 +2,30 @@ DROP VIEW IF EXISTS all_stripe.subscription_renewals;
 CREATE VIEW all_stripe.subscription_renewals AS 
 
 WITH sub_starts AS (
-	SELECT DISTINCT
+	SELECT
+		subscription_id,
 		region,
-		created,
-		id AS subscription_id
-	FROM all_stripe.subscription_history
+		condition,
+		MIN(purchase_date) AS created
+	FROM finance_metrics.contribution_margin
+	WHERE
+		subscription_id IS NOT NULL
+		AND COALESCE(line_item_amount_usd, total_charge_amount_usd) > 0
+	GROUP BY 1,2,3
 )
 
 SELECT
 	sub_starts.region,
 	DATE(sub_starts.created) AS created_date,
 	sub_starts.subscription_id,
-	cm.condition,
+	sub_starts.condition,
 	COUNT(DISTINCT cm.charge_id) AS n_charges_in_first_90d,
-	SUM(COALESCE(cm.line_item_amount_usd, cm.total_charge_amount_usd)) AS amount_usd_in_first_90d
+	SUM(COALESCE(cm.line_item_amount_usd, cm.total_charge_amount_usd, 0)) AS amount_usd_in_first_90d
 FROM sub_starts
 LEFT JOIN finance_metrics.contribution_margin AS cm
-	ON cm.subscription_id = sub_starts.subscription_id
-	AND cm.purchase_date <= DATE_ADD(DATE(sub_starts.created), INTERVAL 90 DAY)
+	ON sub_starts.subscription_id = cm.subscription_id
+	AND sub_starts.condition = cm.condition
+	AND DATE_ADD(DATE(sub_starts.created), INTERVAL 90 DAY) >= cm.purchase_date
+	AND DATE(sub_starts.created) < cm.purchase_date
 GROUP BY 1,2,3,4
+
