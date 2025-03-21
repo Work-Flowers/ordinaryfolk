8d5f90b0fbd1f7edc5a926cf2d9c00ddafd0be3f
denchiuten <d.chiuten@icloud.com>
2025-03-21 11:23:50 +0800

    unioning together the various data sources for HK COD revenue

---
+++ vw_cod_hk_orders_all.sql
@@ -0,0 +1,22 @@
+DROP VIEW IF EXISTS finance_metrics.cod_hk_orders_all;
+CREATE VIEW finance_metrics.cod_hk_orders_all AS 
+
+SELECT
+	a.purchase_date AS date,
+	a.email,
+	LOWER(a.currency) AS currency,
+	a.quantity,
+	a.product_id,
+	a.purchase_amount
+FROM google_sheets.cod_hk_revenue_pre_2025 AS a
+
+UNION ALL
+
+SELECT
+	b.date,
+	b.email,
+	LOWER(b.currency) AS currency,
+	b.quantity,
+	b.product_id,
+	b.revenue AS purchase_amount
+FROM google_sheets.cod_sg_revenue AS b;
