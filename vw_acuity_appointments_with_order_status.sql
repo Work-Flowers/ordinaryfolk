9b6bb40fb21acb2737bb57944ccd37c2f017a868
denchiuten <d.chiuten@icloud.com>
2025-03-25 09:36:15 +0800

    moved QUALIFY statement to CTE to avoid eliminating multiple orders linked to a single appointment

--- vw_acuity_appointments_with_order_status.sql
+++ vw_acuity_appointments_with_order_status.sql
@@ -1,6 +1,28 @@
 DROP VIEW IF EXISTS all_postgres.all_appointments;
 CREATE VIEW all_postgres.all_appointments AS 
 
+WITH first_appt AS (
+	SELECT *
+	FROM all_postgres.acuity_appointment_latest
+	-- only include the first appointment for each customer
+	QUALIFY ROW_NUMBER() OVER (PARTITION BY email ORDER BY date) = 1
+),
+
+first_text_consult AS (
+	SELECT 
+		txt.region,
+		txt.message_id,
+		t.timestamp,
+		t.context_campaign_source,
+		txt.condition
+	FROM segment.text_consultation_booked AS txt
+	INNER JOIN segment.tracks AS t
+		ON txt.message_id = t.message_id
+	-- only include the first appointment for each customer
+	QUALIFY ROW_NUMBER() OVER (PARTITION BY t.user_id ORDER BY t.timestamp) = 1
+)
+
+
 SELECT
 	'Acuity' AS source,
 	appt.region,
@@ -10,20 +32,18 @@ SELECT
 	appt.no_show,
 	o.status,
 	o.prescription_id IS NOT NULL AS has_prescription,
+	o.stripe_subscription_id IS NOT NULL AS has_subscription,
 	JSON_VALUE(o.utm, '$.utmSource') AS utm_source,
-	JSON_VALUE(prod.metadata, '$.condition') AS condition
-FROM all_postgres.acuity_appointment_latest AS appt
+	atc.condition
+FROM first_appt AS appt
 LEFT JOIN all_postgres.order_acuity_appointment AS map
 	ON appt.sys_id = map.acuityappointmentsysid
 LEFT JOIN all_postgres.order AS o
 	ON map.ordersysid = o.sys_id
-LEFT JOIN all_stripe.price AS px
-	ON COALESCE(o.prescription_price_id, o.price_id) = px.id
-LEFT JOIN all_stripe.product AS prod
-	ON px.product_id = prod.id	
--- WHERE DATE(appt.date) <= CURRENT_DATE
--- only include the first appointment for each customer
-QUALIFY ROW_NUMBER() OVER (PARTITION BY appt.email ORDER BY appt.date) = 1
+LEFT JOIN google_sheets.acuity_type_condition_map AS atc
+	ON appt.type = atc.type
+
+
 
 UNION ALL
 
@@ -31,17 +51,14 @@ SELECT
 	'HK Text Consult' AS source,
 	txt.region,
 	txt.message_id AS id,
-	DATE(t.timestamp) AS date,
+	DATE(txt.timestamp) AS date,
 	FALSE AS canceled,
 	FALSE AS no_show,
 	CAST(NULL AS STRING) AS status,
 	FALSE AS has_prescription,
-	t.context_campaign_source AS utm_source,
+	FALSE AS has_subscription,
+	txt.context_campaign_source AS utm_source,
 	map.stripe_condition AS condition
-FROM segment.text_consultation_booked AS txt
-INNER JOIN segment.tracks AS t
-	ON txt.message_id = t.message_id
+FROM first_text_consult AS txt
 LEFT JOIN google_sheets.postgres_stripe_condition_map AS map
 	ON txt.condition = map.postgres_condition
--- only include the first appointment for each customer
-QUALIFY ROW_NUMBER() OVER (PARTITION BY t.user_id ORDER BY t.timestamp) = 1
