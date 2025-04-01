5e14a3003779a87abfef92ff9845512465c7c9bc
denchiuten <d.chiuten@icloud.com>
2025-04-01 12:57:09 +0800

    tableau refresh tasks

---
+++ tableau_jobs.sql
@@ -0,0 +1,17 @@
+SELECT 
+	t.id AS refresh_task_id,
+	COALESCE(t.datasource_id, t.workbook_id) AS object_id,
+	CASE 
+		WHEN t.datasource_id IS NOT NULL THEN 'Data Source'
+		WHEN t.workbook_id  IS NOT NULL THEN 'Workbook'
+		END AS type,
+	COALESCE(ds.name, wb.name) AS name
+FROM tableau_source.extract_refresh_task AS t
+LEFT JOIN tableau_source.data_source AS ds
+	ON t.datasource_id = ds.id
+LEFT JOIN tableau_source.workbook AS wb
+	ON t.workbook_id = wb.id
+WHERE
+	1 = 1
+	AND t._fivetran_deleted IS FALSE
+QUALIFY ROW_NUMBER() OVER (PARTITION BY COALESCE(t.datasource_id, t.workbook_id) ORDER BY t._fivetran_synced DESC) = 1
