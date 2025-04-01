9452752f0813a64e7ac6c5ae1b5d6e55bdd2a6ee
denchiuten <d.chiuten@icloud.com>
2025-04-01 16:43:14 +0800

    use consultation_sessions instead

--- vw_appointments_orders.sql
+++ vw_appointments_orders.sql
@@ -1,73 +1,29 @@
 DROP VIEW IF EXISTS all_postgres.all_appointments;
 CREATE VIEW all_postgres.all_appointments AS 
 
-WITH first_appt AS (
-	SELECT *
-	FROM all_postgres.acuity_appointment_latest
-	-- only include the first appointment for each customer
-	QUALIFY ROW_NUMBER() OVER (PARTITION BY email ORDER BY date) = 1
-),
-
-first_text_consult AS (
-	SELECT 
-		txt.region,
-		txt.message_id,
-		t.timestamp,
-		t.context_campaign_source,
-		txt.condition,
-		txt.order_id
-	FROM segment.text_consultation_booked AS txt
-	INNER JOIN segment.tracks AS t
-		ON txt.message_id = t.message_id
-	-- only include the first appointment for each customer
-	QUALIFY ROW_NUMBER() OVER (PARTITION BY t.user_id ORDER BY t.timestamp) = 1
-)
-
 SELECT
-	'Acuity' AS source,
-	appt.region,
-	CAST(appt.sys_id AS STRING) AS id,
-	DATE(appt.created_at) AS date,
-	appt.canceled,
-	appt.no_show,
-	o.sys_id AS ordersysid,
-	o.status,
+	cs.region,
+	DATE(cs.created_at) AS date,
+	cs.sys_id AS consult_sys_id,
+	cs.consultation_session_type,
+	o.short_id AS order_short_id,
+	cs.order_sys_id,
+	cs.consultation_session_status,
+	cs.progress_status,
+	ca.consult_type,
+	ca.status AS audit_status,
+	ca.evaluation_id AS audit_evaluation_id,
+	cmap.stripe_condition AS condition,
+	o.status AS order_status,
 	o.prescription_id IS NOT NULL AS has_prescription,
 	o.stripe_subscription_id IS NOT NULL AS has_subscription,
 	COALESCE(utm.channel, JSON_VALUE(o.utm, '$.utmSource')) AS utm_source,
-	atc.condition
-FROM first_appt AS appt
-LEFT JOIN all_postgres.order_acuity_appointment AS map
-	ON appt.sys_id = map.acuityappointmentsysid
+FROM all_postgres.consultation_sessions AS cs
 LEFT JOIN all_postgres.order AS o
-	ON map.ordersysid = o.sys_id
-LEFT JOIN google_sheets.acuity_type_condition_map AS atc
-	ON appt.type = atc.type
+	ON cs.order_sys_id = o.sys_id
+LEFT JOIN all_postgres.consultation_audit AS ca
+	ON cs.consultationauditsysid = ca.sys_id
+LEFT JOIN google_sheets.postgres_stripe_condition_map AS cmap
+	ON ca.evaluation_id = cmap.postgres_condition
 LEFT JOIN cac.utm_source_map AS utm
 	ON JSON_VALUE(o.utm, '$.utmSource') = utm.context_campaign_source
-
-
-
-UNION ALL
-
-SELECT
-	'HK Text Consult' AS source,
-	txt.region,
-	txt.message_id AS id,
-	DATE(txt.timestamp) AS date,
-	FALSE AS canceled,
-	FALSE AS no_show,
-	txt.order_id AS ordersysid,
-	o.status,
-	o.prescription_id IS NOT NULL AS has_prescription,
-	o.stripe_subscription_id IS NOT NULL AS has_subscription,
-	COALESCE(utm.channel, txt.context_campaign_source) AS utm_source,
-	map.stripe_condition AS condition
-FROM first_text_consult AS txt
-LEFT JOIN all_postgres.order AS o
-	ON txt.order_id = o.sys_id
-LEFT JOIN google_sheets.postgres_stripe_condition_map AS map
-	ON txt.condition = map.postgres_condition
-LEFT JOIN cac.utm_source_map AS utm
-	ON txt.context_campaign_source = utm.context_campaign_source
-
