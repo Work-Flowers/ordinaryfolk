9ba385ac9f1d93d1699c3979253a11df555daf5e
denchiuten <d.chiuten@icloud.com>
2025-04-04 09:34:19 +0800

    updated tax amount and refund calcs. Included new_existing column

--- vw_cm3_new.sql
+++ vw_cm3_new.sql
@@ -5,6 +5,7 @@ WITH base AS (
   SELECT
     region AS country,
     DATE_TRUNC(purchase_date, MONTH) AS purchase_month,
+    new_existing,
     purchase_type,
     condition,
     customer_id,
@@ -14,7 +15,7 @@ WITH base AS (
     cashback,
     gst_vat,
     fee_rate,
-    refund_rate,
+    amount_refunded_usd,
     MIN(DATE_TRUNC(purchase_date, MONTH)) OVER(PARTITION BY customer_id) AS acq_month
   FROM finance_metrics.contribution_margin
   WHERE customer_id IS NOT NULL
@@ -25,17 +26,19 @@ cm1 AS (
 	  country,
 	  purchase_month,
 	  acq_month,
+	  new_existing,
 	  purchase_type,
 	  condition,
+	  customer_id,
 	  SUM(amount) AS revenue,
 	  SUM(cogs) AS cogs,
 	  SUM(packaging) AS packaging,
 	  SUM(cashback) AS cashback,
-	  SUM(amount * gst_vat) AS gst_vat,
+	  SUM(amount * (1 - 1 / (1+ gst_vat))) AS gst_vat,
 	  SUM(amount * fee_rate) AS payment_gateway_fees,
-	  SUM(amount * refund_rate) AS refunds
+	  SUM(amount_refunded_usd) AS refunds
 	FROM base
-	GROUP BY 1,2,3,4,5
+	GROUP BY 1,2,3,4,5,6,7
 ),
 
 delivery AS (
@@ -71,12 +74,13 @@ SELECT
 	
 	-- prorate delivery costs by revenue within each acquisition month and country
 	SAFE_DIVIDE(cm1.revenue, 
-	  SUM(cm1.revenue) OVER (PARTITION BY cm1.acq_month, cm1.country)
-	) * m.cost AS prorated_marketing_cost
+	  SUM(cm1.revenue) OVER (PARTITION BY cm1.acq_month, cm1.purchase_month, cm1.country)
+	) * COALESCE(m.cost, 0) AS prorated_marketing_cost
 FROM cm1
 LEFT JOIN delivery AS d
 	ON cm1.purchase_month = d.date
 	AND cm1.country = d.country
 LEFT JOIN marketing AS m
 	ON cm1.acq_month = m.date
+	AND cm1.purchase_month = m.date
 	AND cm1.country = m.country
