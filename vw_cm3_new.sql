2d0bde7460b047096977c5f5bb1366683ef7e8ea
denchiuten <d.chiuten@icloud.com>
2025-04-09 14:39:06 +0800

    incorporated google_sheets.opex costs and pro-rate by revenue

--- vw_cm3_new.sql
+++ vw_cm3_new.sql
@@ -22,7 +22,7 @@ WITH base AS (
     amount_refunded_usd,
     MIN(DATE_TRUNC(purchase_date, MONTH)) OVER(PARTITION BY customer_id) AS acq_month
   FROM finance_metrics.contribution_margin
-  WHERE customer_id IS NOT NULL
+--   WHERE customer_id IS NOT NULL
 ),
 
 cm1 AS (
@@ -68,7 +68,25 @@ marketing AS (
 	FROM cac.marketing_spend
 	GROUP BY 1,2
 
+),
+
+opex AS (
+	SELECT
+		DATE_TRUNC(o.date, MONTH) AS date,
+		LOWER(o.country) AS country,
+		-SUM(o.teleconsultation_fees / fx.fx_to_usd) AS teleconsultation_fees,
+		-SUM(o.dispensing_fees / fx.fx_to_usd) AS dispensing_fees,
+		-SUM(o.operating_expense / fx.fx_to_usd) AS operating_expense,
+		-SUM(o.staff_cost / fx.fx_to_usd) AS staff_cost
+	FROM google_sheets.opex AS o
+	LEFT JOIN ref.fx_rates AS fx
+		ON LOWER(o.currency) = fx.currency
+	WHERE
+		o.date IS NOT NULL
+	GROUP BY 1,2
 )
+
+
 
 SELECT
 	cm1.*,
@@ -83,7 +101,28 @@ SELECT
 	-- prorate delivery costs by revenue within each acquisition month and country
 	SAFE_DIVIDE(cm1.revenue, 
 	  SUM(cm1.revenue) OVER (PARTITION BY cm1.acq_month, cm1.purchase_month, cm1.country)
-	) * COALESCE(m.cost, 0) AS prorated_marketing_cost
+	) * COALESCE(m.cost, 0) AS prorated_marketing_cost,
+	
+	-- prorate teleconsultation_fees by revenue within each purchase month and country
+	SAFE_DIVIDE(cm1.revenue, 
+	  SUM(cm1.revenue) OVER (PARTITION BY cm1.purchase_month, cm1.country)
+	) * opex.teleconsultation_fees AS teleconsultation_fees,
+	
+	-- prorate dispensing_fees by revenue within each purchase month and country
+	SAFE_DIVIDE(cm1.revenue, 
+	  SUM(cm1.revenue) OVER (PARTITION BY cm1.purchase_month, cm1.country)
+	) * opex.dispensing_fees AS dispensing_fees,
+	
+	-- prorate operating_expense by revenue within each purchase month and country
+	SAFE_DIVIDE(cm1.revenue, 
+	  SUM(cm1.revenue) OVER (PARTITION BY cm1.purchase_month, cm1.country)
+	) * opex.operating_expense AS operating_expense,
+	
+	-- prorate staff_cost by revenue within each purchase month and country
+	SAFE_DIVIDE(cm1.revenue, 
+	  SUM(cm1.revenue) OVER (PARTITION BY cm1.purchase_month, cm1.country)
+	) * opex.staff_cost AS staff_cost
+	
 FROM cm1
 LEFT JOIN delivery AS d
 	ON cm1.purchase_month = d.date
@@ -92,3 +131,6 @@ LEFT JOIN marketing AS m
 	ON cm1.acq_month = m.date
 	AND cm1.purchase_month = m.date
 	AND cm1.country = m.country
+LEFT JOIN opex
+	ON cm1.purchase_month = opex.date
+	AND cm1.country = opex.country
