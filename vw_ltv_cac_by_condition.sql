1f90f71717374f79ca1f7ff0ff8bcc253466b8ff
denchiuten <d.chiuten@icloud.com>
2025-04-23 14:25:38 +0800

    incorporated condition column and updated PARTITION definitions to reflect it

--- vw_ltv_cac_by_condition.sql
+++ vw_ltv_cac_by_condition.sql
@@ -1,34 +1,37 @@
 DROP VIEW IF EXISTS finance_metrics.ltv_cac_by_condition;
-CREATE VIEW finance_metrics.ltv_cac AS 
+CREATE VIEW finance_metrics.ltv_cac_by_condition AS 
 
 WITH all_mrr AS (
 	SELECT
 		region,
 		obs_date,
-		SUM(CASE WHEN lifecyle = 'New' THEN n_customers ELSE 0 END) AS n_new_customers,
+		condition,
+		SUM(CASE WHEN lifecyle = 'New' THEN n_subscriptions ELSE 0 END) AS n_new_subscriptions,
 		SUM(current_mrr) AS current_mrr,
-		SUM(n_customers) AS current_n_customers
-	FROM finance_metrics.customer_lifecyle_monthly
+		SUM(n_subscriptions) AS current_n_subscriptions
+	FROM finance_metrics.subscription_lifecyle_monthly
 	WHERE 
 		current_mrr > 0
-	GROUP BY 1,2
+	GROUP BY 1,2,3
 ),
 
 churn_info AS (
 	SELECT
 		region,
 		obs_date,
-		n_customers AS n_churned_customers,
+		condition,
+		n_subscriptions AS n_churned_subscriptions,
 		SUM(lagged_mrr) AS churned_mrr		
-	FROM finance_metrics.customer_lifecyle_monthly
+	FROM finance_metrics.subscription_lifecyle_monthly
 	WHERE lifecyle = 'Churn'
-	GROUP BY 1,2,3
+	GROUP BY 1,2,3,4
 ),
 
 gm_inputs AS (
 	SELECT
 		country,
 		date,
+		condition,
 		amount - refunds - gst_vat AS net_revenue,
 		cogs,
 		marketing_cost
@@ -37,14 +40,14 @@ gm_inputs AS (
 
 SELECT	
 	am.*,
-	ci.n_churned_customers,
+	ci.n_churned_subscriptions,
 	ci.churned_mrr,
-	LAG(am.current_n_customers) OVER(
-		PARTITION BY am.region 
+	LAG(am.current_n_subscriptions) OVER(
+		PARTITION BY am.region, am.condition 
 		ORDER BY am.obs_date
-	) AS base_n_customers,
+	) AS base_n_subscriptions,
 	LAG(am.current_mrr) OVER(
-		PARTITION BY am.region 
+		PARTITION BY am.region , am.condition
 		ORDER BY am.obs_date
 	) AS base_mrr,
 	gm.net_revenue,
@@ -54,7 +57,9 @@ FROM all_mrr AS am
 LEFT JOIN churn_info AS ci
 	ON am.obs_date = ci.obs_date
 	AND am.region = ci.region
+	AND am.condition = ci.condition
 LEFT JOIN gm_inputs AS gm
 	ON am.obs_date = gm.date
 	AND am.region = gm.country
+	AND am.condition = gm.condition
 ORDER BY 1,2
