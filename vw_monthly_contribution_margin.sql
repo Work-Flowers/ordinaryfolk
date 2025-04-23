1f25b1fd8804542fc6b3317e71458da5eb38ac9d
denchiuten <d.chiuten@icloud.com>
2025-04-23 20:59:11 +0800

    correction of tax amount calculation; inclusion of COGS for teleconsult transactions

--- vw_monthly_contribution_margin.sql
+++ vw_monthly_contribution_margin.sql
@@ -5,18 +5,41 @@ WITH cm1 AS (
 	SELECT
 		region AS country,
 		DATE_TRUNC(purchase_date, MONTH) AS date,
-		purchase_type,
 		COALESCE(condition, 'N/A') AS condition,
 		SUM(COALESCE(line_item_amount_usd, total_charge_amount_usd)) AS amount,
 		SUM(cogs) AS cogs,
 		SUM(packaging) AS packaging,
 		SUM(cashback) AS cashback,
-		SUM(COALESCE(line_item_amount_usd, total_charge_amount_usd) * gst_vat) AS gst_vat,
+		-- correct the calculation of tax amount paid, since Stripe line item and charge amounts are inclusive of tax
+		SUM(
+			(COALESCE(line_item_amount_usd, total_charge_amount_usd) - amount_refunded_usd)  * (1 - 1 / (1 + gst_vat))
+		) AS gst_vat,
 		SUM(COALESCE(line_item_amount_usd, total_charge_amount_usd) * fee_rate) AS payment_gateway_fees,
 		SUM(cm.amount_refunded_usd) AS refunds
 	FROM finance_metrics.contribution_margin AS cm
 	WHERE purchase_type = 'Subscription'
-	GROUP BY 1,2,3,4
+	GROUP BY 1,2,3
+),
+
+cm1_with_teleconsult_fees AS (
+	SELECT
+		cm1.country,
+		cm1.date,
+		cm1.condition,
+		cm1.amount,
+		COALESCE(cm1.cogs, -op.teleconsultation_fees / fx.fx_to_usd) AS cogs,
+		cm1.packaging,
+		cm1.cashback,
+		cm1.gst_vat,
+		cm1.payment_gateway_fees,
+		cm1.refunds
+	FROM cm1
+	LEFT JOIN google_sheets.opex AS op
+		ON cm1.date = op.date
+		AND cm1.country = op.country
+		AND cm1.condition = 'Services'
+	LEFT JOIN ref.fx_rates AS fx
+		ON LOWER(op.currency) = fx.currency
 ),
 
 delivery AS (
@@ -42,14 +65,14 @@ marketing AS (
 )
 
 SELECT
-	cm1.*,
-	delivery.cost * cm1.amount / SUM(cm1.amount) OVER (PARTITION BY cm1.date, cm1.country) AS delivery_cost,
-	mar.cost * cm1.amount / SUM(cm1.amount) OVER (PARTITION BY cm1.date, cm1.country, cm1.condition) AS marketing_cost
-FROM cm1
+	cwt.*,
+	delivery.cost * cwt.amount / SUM(cwt.amount) OVER (PARTITION BY cwt.date, cwt.country) AS delivery_cost,
+	mar.cost * cwt.amount / SUM(cwt.amount) OVER (PARTITION BY cwt.date, cwt.country, cwt.condition) AS marketing_cost
+FROM cm1_with_teleconsult_fees AS cwt
 LEFT JOIN delivery
-	ON cm1.date = delivery.date
-	AND cm1.country = delivery.country
+	ON cwt.date = delivery.date
+	AND cwt.country = delivery.country
 LEFT JOIN marketing AS mar
-	ON cm1.date = mar.date
-	AND cm1.country = mar.country
-	AND cm1.condition = mar.condition
+	ON cwt.date = mar.date
+	AND cwt.country = mar.country
+	AND cwt.condition = mar.condition
