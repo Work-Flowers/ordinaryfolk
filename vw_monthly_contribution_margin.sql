46afbc76ac6901e6d9df327ebbd7369cefb3a70f
denchiuten <d.chiuten@icloud.com>
2025-04-23 14:12:22 +0800

    added condition granularity

--- vw_monthly_contribution_margin.sql
+++ vw_monthly_contribution_margin.sql
@@ -6,6 +6,7 @@ WITH cm1 AS (
 		region AS country,
 		DATE_TRUNC(purchase_date, MONTH) AS date,
 		purchase_type,
+		COALESCE(condition, 'N/A') AS condition,
 		SUM(COALESCE(line_item_amount_usd, total_charge_amount_usd)) AS amount,
 		SUM(cogs) AS cogs,
 		SUM(packaging) AS packaging,
@@ -15,7 +16,7 @@ WITH cm1 AS (
 		SUM(cm.amount_refunded_usd) AS refunds
 	FROM finance_metrics.contribution_margin AS cm
 	WHERE purchase_type = 'Subscription'
-	GROUP BY 1,2,3
+	GROUP BY 1,2,3,4
 ),
 
 delivery AS (
@@ -33,16 +34,17 @@ marketing AS (
 	SELECT
 		DATE_TRUNC(date, MONTH) AS date,
 		LOWER(country_code) AS country,
+		COALESCE(condition, 'N/A') AS condition,
 		SUM(cost_usd) AS cost
 	FROM cac.marketing_spend
-	GROUP BY 1,2
+	GROUP BY 1,2,3
 
 )
 
 SELECT
 	cm1.*,
 	delivery.cost * cm1.amount / SUM(cm1.amount) OVER (PARTITION BY cm1.date, cm1.country) AS delivery_cost,
-	mar.cost * cm1.amount / SUM(cm1.amount) OVER (PARTITION BY cm1.date, cm1.country) AS marketing_cost
+	mar.cost * cm1.amount / SUM(cm1.amount) OVER (PARTITION BY cm1.date, cm1.country, cm1.condition) AS marketing_cost
 FROM cm1
 LEFT JOIN delivery
 	ON cm1.date = delivery.date
@@ -50,3 +52,4 @@ LEFT JOIN delivery
 LEFT JOIN marketing AS mar
 	ON cm1.date = mar.date
 	AND cm1.country = mar.country
+	AND cm1.condition = mar.condition
