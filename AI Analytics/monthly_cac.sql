75d181f1be43832af534a886b058d3f967d39ca3
denchiuten <d.chiuten@icloud.com>
2025-05-09 11:51:57 +0800

    go back 13 months instead of 12

--- AI Analytics/monthly_cac.sql
+++ AI Analytics/monthly_cac.sql
@@ -1,22 +1,20 @@
 WITH marketing AS (
 	SELECT
 		DATE_TRUNC(date, MONTH) AS date,
-		COALESCE(condition, 'N/A') AS condition,
 		LOWER(country_code) AS country,
 		ROUND(SUM(cost_usd), 0) AS spend,
 		ROUND(SUM(impressions), 0) AS impressions,
 		ROUND(SUM(clicks), 0) AS clicks
 	FROM cac.marketing_spend
 	WHERE country_code IS NOT NULL
-	GROUP BY 1,2,3
+	GROUP BY 1,2
 ),
 
 all_keys AS (
 	
 	SELECT DISTINCT 
 		date,
-		country,
-		condition
+		country
 	FROM marketing
 	
 	UNION DISTINCT
@@ -24,7 +22,6 @@ all_keys AS (
 	SELECT DISTINCT
 		DATE_TRUNC(purchase_date, MONTH) AS date,
 		region AS country,
-		COALESCE(condition, 'N/A') AS condition,
 	FROM finance_metrics.contribution_margin
 	WHERE region IS NOT NULL
 )
@@ -33,19 +30,17 @@ all_keys AS (
 SELECT 
 	k.date,
 	k.country,
-	k.condition,
 	COALESCE(marketing.spend, 0) AS marketing_spend,
 	COUNT(DISTINCT CASE WHEN cm.new_existing = 'New' THEN cm.customer_id END) AS n_new_customers
 FROM all_keys AS k
-LEFT JOIN finance_metrics.contribution_margin AS cm
-	ON k.date = cm.purchase_date
-	AND k.country = cm.region
-	AND k.condition = COALESCE(cm.condition, 'N/A')
+LEFT JOIN finance_metrics.monthly_contribution_margin AS cm
+	ON k.date = cm.date
+	AND k.country = cm.country
+-- 	AND cm.purchase_type = 'Subscription'
 LEFT JOIN marketing		
 	ON k.date = marketing.date
 	AND k.country = marketing.country
-	AND k.condition = marketing.condition
 WHERE 
 	k.date >= DATE_SUB(DATE_TRUNC(CURRENT_DATE, MONTH), INTERVAL 12 MONTH)
 	AND k.date < DATE_TRUNC(CURRENT_DATE, MONTH)
-GROUP BY 1,2,3,4
+GROUP BY 1,2,3
