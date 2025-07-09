8f7d62b2fcd89358cd66d65be1b5b2a7a53ad173
Dennis Chiuten <dennis@work.flowers>
2025-07-09 18:00:34 +0800

    Update direct_variable_cost.sql

--- direct_variable_cost.sql
+++ direct_variable_cost.sql
@@ -3,30 +3,28 @@ CREATE VIEW finance_metrics.direct_variable_cost AS
 
 WITH payment_gateway_fees AS (
 	SELECT
-		cm.region AS country,
-		DATE(DATE_TRUNC(cm.purchase_date, MONTH)) AS date,
-		SUM(cm.fee_rate * cm.line_item_amount_usd) AS cost
-	FROM finance_metrics.contribution_margin AS cm
+		cm.country,
+		cm.date,
+		SUM(cm.gateway_fees) AS cost
+	FROM finance_metrics.monthly_contribution_margin AS cm
 	GROUP BY 1,2
 ),
 
 packaging AS (
 	SELECT
-		cm.region AS country,
-		DATE(DATE_TRUNC(cm.purchase_date, MONTH)) AS date,
+		cm.country,
+		cm.date,
 		SUM(cm.packaging) AS cost		
-	FROM finance_metrics.contribution_margin AS cm
+	FROM finance_metrics.monthly_contribution_margin AS cm
 	GROUP BY 1,2
 ),
 
 delivery AS (
 	SELECT
-		DATE_TRUNC(dc.date, MONTH) AS date,
-		dc.country,
-		SUM(dc.cost / fx.fx_to_usd) AS cost
-	FROM google_sheets.delivery_cost AS dc
-	LEFT JOIN ref.fx_rates AS fx
-		ON LOWER(dc.currency) = LOWER(fx.currency)
+		cm.country,
+		cm.date,
+		SUM(cm.delivery_cost) AS cost		
+	FROM finance_metrics.monthly_contribution_margin AS cm
 	GROUP BY 1,2
 )
 
